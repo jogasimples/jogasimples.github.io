8c588b5a55908b81dbfb4a1e000960c8512c6ab5
jogasimples <47523257+jogasimples@users.noreply.github.com>
2026-08-18 12:36:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var content_data = {
   },
   "profile": {
     "name": "JOGA SIMPLES",
-    "avatarText": "TV",
+    "avatarText": "JOGA",
     "avatarImage": "avatar2.png",
     "avatarLink": "https://www.instagram.com/jogasimplespt",
     "disclaimer": "Treinador de futebol Nivel II (UEFA B) com 10 anos de treino e durante 20 anos como jogador de futebol. Atualmente parceiro da Twitch como criador de conteúdos relacionados com futebol. Entender o processo e as dinâmicas que as equipas apresentam na nossa Liga BWIN. Analisa as equipas, opina sobre o momento e tudo aquilo que está por dentro das quatro linhas."
